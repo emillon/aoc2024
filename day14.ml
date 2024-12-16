343522072c4778604b7827792ed5dd982896af3d
Etienne Millon <me@emillon.org>
2024-12-16 14:59:26 +0100

    day 14 p2

--- day14.ml
+++ day14.ml
@@ -16,12 +16,7 @@ let parse =
      { p; v })
 ;;
 
-let sim100 t ~dimx ~dimy =
-  let l =
-    List.map t ~f:(fun { p; v } ->
-      let x, y = Vec.add p (Vec.smul v 100) in
-      x % dimx, y % dimy)
-  in
+let danger l ~dimx ~dimy =
   let midx = (dimx - 1) / 2 in
   let midy = (dimy - 1) / 2 in
   let low_x (x, _) = x < midx in
@@ -35,7 +30,37 @@ let sim100 t ~dimx ~dimy =
   q1 * q2 * q3 * q4
 ;;
 
+let sim100 t ~dimx ~dimy =
+  List.map t ~f:(fun { p; v } ->
+    let x, y = Vec.add p (Vec.smul v 100) in
+    x % dimx, y % dimy)
+  |> danger ~dimx ~dimy
+;;
+
 let p1 = sim100 ~dimx:101 ~dimy:103
+
+let p2 t0 =
+  let dimx = 101 in
+  let dimy = 103 in
+  let r = ref t0 in
+  let lowest_danger = ref Int.max_value in
+  let lowest_danger_i = ref Int.max_value in
+  for i = 0 to dimx * dimy do
+    let t = !r in
+    let d = danger (List.map t ~f:(fun p -> p.p)) ~dimx ~dimy in
+    if d < !lowest_danger
+    then (
+      lowest_danger := d;
+      lowest_danger_i := i);
+    r
+    := List.map t ~f:(fun { p; v } ->
+         let x, y = Vec.add p v in
+         let p = x % dimx, y % dimy in
+         { p; v })
+  done;
+  !lowest_danger_i
+;;
+
 let f1 s = parse s |> p1
-let f2 _ = 0
+let f2 s = parse s |> p2
 let run () = Run.run ~f1 ~f2 Day14_input.data
